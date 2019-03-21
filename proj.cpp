7a86af888b1d0e15489e54f7c58d9dc0bf3b9527
TissueRoll <christopher.dizon@protonmail.com>
2019-03-21 19:50:58 +0800

    untested code but probably ok

--- proj.cpp
+++ proj.cpp
@@ -27,10 +27,35 @@ void FCFS(Process p[], int n) {
 }
 
 void SJF(Process p[], int n) {
-	
+	priority_queue<Process, vector<Process>, 
+		function<bool(Process,Process)> > pq
+		([](const Process &a, const Process &b) -> bool {
+		if (a.length == b.length) return a.p_id < b.p_id;
+		return a.length < b.length;
+	});
+	sort(p, p+n, [](const Process &a, const Process &b) -> bool{ // sort it by arrival time, and then by length
+		if (a.arrival == b.arrival) {
+			if (a.length == b.length) return a.p_id < b.p_id;
+			else return a.length < b.length;
+		}
+		return a.arrival < b.arrival;
+	});
+	int index = 0;
+	int current_time = p[0].arrival;
+	for (;current_time == p[index].arrival; index++) pq.push(p[index]);
+	while (!pq.empty()) {
+		Process t = pq.top(); pq.pop(); // current running process
+		cout << current_time << ' ' << t.p_id << ' ' << t.length << "X\n";
+		for (; index < n; index++) {
+			if (current_time + t.length < p[index].arrival) break;
+			pq.push(p[index]);
+		}
+		current_time += t.length;
+		if (pq.size() == 0 && index < n) pq.push(p[index++]);
+	}
 }
 
-void P(Process p[], int n) {
+void P(Process p[], int n) { // not done
 	sort(p, p+n, [](const Process &a, const Process &b) -> bool{
 		// arrival, then prio, then index
 		if (a.arrival == b.arrival) return a.p_id < b.p_id;
@@ -58,36 +83,36 @@ void SRTF(Process p[], int n) {
 		}
 		return a.arrival < b.arrival;
 	});
-	int cur_time = p[0].arrival; // get the earliest time
 	int index = 0;
-	for(; p[index].arrival == cur_time && index < n; index++) pq.push(p[index]);
-	while (!pq.empty()) {
-		Process t = pq.top(); pq.pop();
-		if (pq.size() > 0) {
-			Process nt = pq.top();
-			if (cur_time+t.length <= nt.arrival) {
-				cout << cur_time << ' ' << t.p_id << ' ' << cur_time+t.length << "X\n";
-				cur_time = nt.arrival;
-			} else if (cur_time+t.length-nt.arrival <= nt.length) {
-				cout << cur_time << ' ' << t.p_id << ' ' << cur_time+t.length << "X\n";
-				cur_time += t.length;
-			} else {
-				cout << cur_time << ' ' << t.p_id << ' ' << nt.arrival << "\n";
-				pq.push({nt.arrival, cur_time+t.length-nt.arrival, t.priority, t.p_id});
-				cur_time = nt.arrival;
+	int current_time = p[0].arrival;
+	bool ok = 0;
+	for (;current_time == p[index].arrival; index++) pq.push(p[index]);
+	while (!pq.empty()) { // pq will contain processes that are currently in the ready queue
+		ok = 0;
+		Process t = pq.top(); pq.pop(); // current running process
+		for (; index < n; index++) { // go through the array if there still more to process
+			if (current_time + t.length < p[index].arrival) {
+				cout << current_time << ' ' << t.p_id << ' ' << t.length << endl;
+				current_time += t.length;
+				ok = 1;
+				break;
 			}
-		} else if (index < n) { // assumption here is wala na sa shit
-			bool first_higher = 0;
-			// pq.push(t);
-			for(; !first_higher && index < n; index++) {
-				if (cur_time+t.length > p[index].arrival) first_higher = 1;
+			if (current_time + t.length - p[index].arrival > p[index].length) { // when the new process is better
+				cout << current_time << ' ' << t.p_id << ' ' << current_time + t.length - p[index].arrival << endl;
+				pq.push(p[index]);
+				pq.push({p[index].arrival, current_time + t.length - p[index].arrival, t.priority, t.p_id});
+				current_time = p[index].arrival;
+				ok = 1;
+				break;
+			} else {
 				pq.push(p[index]);
 			}
-			
-		} else {
-			cout << cur_time << ' ' << t.p_id << ' ' << t.length << "X\n";
 		}
-		
+		if (!ok) {
+			cout << current_time << ' ' << t.p_id << ' ' << t.length << "X\n";
+			current_time += t.length;
+		}
+		if (pq.size() == 0 && index < n) pq.push(p[index++]); // if empty but theres still some processes
 	}
 	
 }
